9aebc237ba29f4b140a8f5bd9ef5ed5c4b2b3559
Devon DeJohn <ddejohn@msudenver.edu>
2019-10-15 20:07:35 -0600

    minor fixes

--- hw7.jl
+++ hw7.jl
@@ -39,13 +39,14 @@ function integrate(; f, mthd, intvl::Tuple, n::Int, deg=3)
     a, b = intvl # unpack the tuple
     a, b = minmax(a, b) # rearrange so a, b correspond to the correct endpoints
     subs = collect(a:(b-a)/n:b) # construct an array of sub intervals
+    k = length(subs)-1
 
     if string(mthd) == "gauss"
-        return sum(mthd(f=f, a=subs[i], b=subs[i+1], n=deg) for i in 1:length(subs)-1)
+        return sum(mthd(f=f, a=subs[i], b=subs[i+1], n=deg) for i in 1:k)
     end
 
     # composite area sum using given 'mthd'
-    return sum(mthd(f=f, a=subs[i], b=subs[i+1]) for i in 1:length(subs)-1)
+    return sum(mthd(f=f, a=subs[i], b=subs[i+1]) for i in 1:k)
 end
 
 
@@ -71,8 +72,8 @@ function error_plot(; f, exact, intvl)
     
     nvals = [2, 4, 6, 8, 16, 32, 64]
     
-    p = plot([], [], label="", title="absolute error vs # subintervals", size=(900,900),
-        xlabel="# subintervals", ylabel="absolute error",
+    p = plot([], [], label="", title="absolute error vs # subintervals",
+        size=(900,900), xlabel="# subintervals", ylabel="absolute error",
         xticks=nvals, xaxis=:log, xformatter=x -> round(x), yaxis=:log)
     
     for n in nvals
@@ -93,4 +94,4 @@ function error_plot(; f, exact, intvl)
     plot!(p, nvals, gss_err, lc=:cyan, lw=4, label="gauss")
     
     p
-end;
+end
