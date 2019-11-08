b0bb611ab15dbc7372223510256e8f0be054a928
Devon DeJohn <ddejohn@msudenver.edu>
2019-11-07 21:47:43 -0700

    minor simplification

--- MTH_4480_SRC/hw10.jl
+++ MTH_4480_SRC/hw10.jl
@@ -10,25 +10,24 @@ function power_method(A, x, tol=1.0e-8)
 
     while true
         x0 = x
-        normalize!(x)
-        xn = A*x
+        xn = A*normalize(x)
         x = normalize(xn)
         mu = x'*xn
 
         n += 1
         err = norm(x0-x, Inf)
         push!(err_plot, err)
-        
+
         if err < tol
             break
         end
     end
 
     println("eigenvector: $(x)\neigenvalue: $(mu)\niterations: $(n)")
-    
+
     plot([x for x in 1:n], err_plot,
         yaxis=:log, lw=4, lc=:teal, label="",
-        ylabel="error", xlabel="iterations",
+        ylabel="absolute error", xlabel="iterations",
         size=(900,900), title="Maximum eigenvalue error vs number of iterations")
     scatter!([x for x in 1:n], err_plot, mc=:gold, label="")
 end;
